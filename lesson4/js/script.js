0f3344b51231660baa83ad0277af717884b03a9f
RSwenlin <Robyn796@mac.com>
2021-05-16 14:52:29 -0600

    date

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,7 +1,8 @@
+document.querySelector("#theyear").innerHTML = date.getFullYear();
+
 const datefield =
 document.querySelector("date");
 const now = new Date();
 const fulldate = new
 Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
-document.querySelector("#theyear").innerHTML = date.getFullYear();
