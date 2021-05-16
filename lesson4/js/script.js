510d4c7914d5bae4a29831fe836e773f8ebe0134
RSwenlin <Robyn796@mac.com>
2021-05-16 14:38:36 -0600

    date

---
+++ lesson4/js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    
+}
+const datefield =
+document.querySelector("date");
+const now = new Date();
+const fulldate = new
+Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
