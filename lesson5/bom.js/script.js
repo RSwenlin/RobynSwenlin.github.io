f3cc189172a269e239dd96429dca3d634693e3e1
RSwenlin <Robyn796@mac.com>
2021-05-20 13:44:07 -0600

    li

--- lesson5/bom.js/script.js
+++ lesson5/bom.js/script.js
@@ -4,9 +4,14 @@ const output = document.querySelector('.list');
 
 button.addEventListener('click', () => {
     if (fav.value !=''){
+        // create the element in the list
         let li = createElement('li');
         let deletebutton = document.createElement('button');
+        // change some properties ... textcontent
         li.textContent = fav.value;
         deletebutton.textContent = '❌';
+        // add the button to the li
+        li.appendChild(deletebutton);
+        output.append(li);
 }
 });
