2a6a1aca51ae4881f258a407f25a2c22e53cf3cc
RSwenlin <Robyn796@mac.com>
2021-05-30 19:39:45 -0600

    changes

--- js/js/script.js
+++ js/js/script.js
@@ -3,10 +3,4 @@ const date = new Date();
 document.querySelector("#theyear").innerHTML = date.getFullYear();
 document.querySelector("#lastmod").innerHTML = document.lastModified;
 
-WebFont.load({
-    google: {
-      families: [
-         'fontfamilynameofyourchoice'
-      ]
-    }
-  });
+
--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -8,17 +8,17 @@ hambutton.addEventListener("click", () =>
 
 
 
-const now = new Date().getDay;
-if (day = fri) {
-    document.querySelector("banner").innerHTML = "&#129374Pancake day on Fridays only!!";
+const thedate = new Date();
+if (thedate.getDay() != 5) {
+    document.querySelector(".banner").innerHTML = "&#129374Pancake day on Fridays only!!";
   }
   const date = new Date();
   document.querySelector("#theyear").innerHTML = date.getFullYear();
 
-  const datefield =
-document.querySelector("date");
-const now = new Date();
-const fulldate = new
-Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
-datefield.textContent= fulldate;
-  
+
+  //const datefield =
+ // document.querySelector("date");
+  //const now = new Date();
+ // const fulldate = new
+ // Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+ // datefield.textContent= fulldate;
