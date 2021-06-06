38e84c801eee7933f9512d5a35d7565d843eaa00
RSwenlin <Robyn796@mac.com>
2021-06-06 14:31:05 -0600

    pic js

--- lesson7/lazyloadjs/script.js
+++ lesson7/lazyloadjs/script.js
@@ -1,3 +1,19 @@
+const images = document.querySelectorAll("[data-src]");
+
+const imgOptions = {};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) =>  {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+
+        }
+    })
+
+}, imgOptions);
+
+
 
 const date = new Date();
 document.querySelector("#theyear").innerHTML = date.getFullYear();
