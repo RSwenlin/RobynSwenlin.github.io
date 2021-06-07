63677b61432f5d11445a1c3d591d24fc173d5a79
RSwenlin <Robyn796@mac.com>
2021-06-07 08:58:51 -0600

    windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,7 @@
 
 
-const temp =document.querySelector(".temp").innerHTML;
-const wind =document.querySelector(".wind").innerHTML;
+const temp = parseFloat(document.querySelector("#temp").textContent);
+const wind = parseFloat(document.querySelector("#wind").textContent);
 
 const windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * (Math.pow(wind, 0.16)))
 
