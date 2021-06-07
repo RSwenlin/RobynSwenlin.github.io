5066b2e35c474f679b4a6bc00447dd68ed746ecd
RSwenlin <Robyn796@mac.com>
2021-06-07 16:06:58 -0600

    css add

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,13 +3,13 @@
 const temp = parseFloat(document.querySelector("#temp").textContent);
 const wind = parseFloat(document.querySelector("#wind").textContent);
 
-const windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * (Math.pow(wind, 0.16)))
+let windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * (Math.pow(wind, 0.16)))
 
 if (temp <= 50 && wind > 3) {
   windChill = Math.round(windchill);
 }
 else {
-    windChill = "N/A";
+    windChill = ("N/A");
 }
 document.querySelector("#windchill").textContent= windChill;
 
