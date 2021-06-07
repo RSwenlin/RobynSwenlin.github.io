03b1e0016fb184678e8feb482518282c723f8c52
RSwenlin <Robyn796@mac.com>
2021-06-07 10:56:38 -0600

    wrong spot

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,12 +3,14 @@
 const temp = parseFloat(document.querySelector("#temp").textContent);
 const wind = parseFloat(document.querySelector("#wind").textContent);
 
-const windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * (Math.pow(wind, 0.16)))
+const windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * (Math.pow(wind, 0.16)))
 
 if (temp <= 50 && wind > 3) {
-    windchill = math.round(windchill);
+  windChill = Math.round(windchill);
 }
 else {
-    windchill = "N/A";
+    windChill = "N/A";
 }
-document.querySelector("#windchill").textContent= windchill;
+document.querySelector("#windchill").textContent= windChill;
+
+
