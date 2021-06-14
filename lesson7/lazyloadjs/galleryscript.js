83c6e3837f3e192a9e14fdafae0375fa870af091
RSwenlin <Robyn796@mac.com>
2021-06-13 19:49:49 -0600

    # visited

--- lesson7/lazyloadjs/galleryscript.js
+++ lesson7/lazyloadjs/galleryscript.js
@@ -6,3 +6,8 @@ const imagesToLoad = document.querySelectorAll("img[gallery-images]");
 
 
 if ("IntersectionObserver" in window) {
+
+    // get the date of the last visit from local storage
+    const lastVisit = localStorage.getItem("lastvisit") || Date.now();
+    document.querySelector("#lastvisit").textContent = (lastVisit - Date.now()) / 864000;
+    localStorage.setItem("lastvisit", Data.now());
