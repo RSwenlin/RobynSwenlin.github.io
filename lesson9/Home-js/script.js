56970851da944349ef95199659b64957623c21f1
RSwenlin <Robyn796@mac.com>
2021-06-21 12:50:30 -0600

    wireframe

--- lesson9/Home-js/script.js
+++ lesson9/Home-js/script.js
@@ -1,3 +1,29 @@
+
+
+    const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+    fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+  const towns = jsonObject['towns'];
+  for (let i = 0; i < towns.length; i++ ) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let img = document.createElement('img');
+
+    h2.textContent = `${towns[i].name}`;
+    img.setAttribute('src', towns[i].sodaspring.jpg);
+
+
+
+
+    card.append(h2);
+    card.append(img);
+    document.querySelector('div.cards').append(card);
+  }
+});
 const date = new Date();
 document.querySelector("#theyear").innerHTML = date.getFullYear();
 document.querySelector("#lastmod").innerHTML = document.lastModified;
@@ -6,23 +32,3 @@ document.querySelector("#lastmod").innerHTML = document.lastModified;
     const lastVisit = localStorage.getItem("lastvisit") || Date.now();
     document.querySelector("#lastvisit").textContent = (lastVisit - Date.now()) / 864000;
     localStorage.setItem("lastvisit", Data.now());
-
-    const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-    fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject)
-  const towns = jsonObject['towns'];
-  for (let i = 0; i < towns.length; i++ ) {
-    let card = document.createElement('section');
-
-
-
-
-
-
-    document.querySelector('div.cards').append(card);
-  }
-});
