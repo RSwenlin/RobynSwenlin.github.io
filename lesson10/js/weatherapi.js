20e3f7195d58d3cb51855c27d5103ce5af801eae
RSwenlin <Robyn796@mac.com>
2021-06-27 14:29:13 -0600

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,5 +1,12 @@
-const apiURL =
+const cityid = "5604473";
+const APPID = "36e656b2905c89b5bb9203c9be1b4077";
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
 
 fetch(apiURL)
     .then((response) => response.json())
-    .then((JsObject) => )
+    .then((jsObject) => {
+        const temperature = document.querySelector('#temperature');
+        temperature.textContent = jsObject.main.temp;
+        const imagesrc =`https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    }
