a750b8b12c6d17ddee02968c8dcbb90e30d51be1
RSwenlin <Robyn796@mac.com>
2021-07-01 14:43:23 -0600

    home page

--- lesson11/js/script.js
+++ lesson11/js/script.js
@@ -16,15 +16,18 @@ fetch(requestURL)
     town.forEach((x) => {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
+      let h3 = document.createElement("h3");
       let img = document.createElement("img");
       let p = document.createElement("p");
 
-      h2.innerHTML = `${x.name} <br> Town Motto "${x.motto}"`;
+      h2.innerHTML = `${x.name}`;
+      h3.innerHTML = `Town Motto "${x.motto}"`;
 
-      p.innerHTML = `The town was founded in ${x.yearFounded}. <br> The current population is ${x.currentPopulation}. <br> The average rainfall is ${x.averageRainfall} in.`;
+      p.innerHTML = `Town was founded in ${x.yearFounded}. <br> Current population is ${x.currentPopulation}. <br> Average rainfall is ${x.averageRainfall} in.`;
       img.setAttribute("src", `images/${x.photo}`);
 
       card.append(h2);
+      card.append(h3);
       card.append(img);
       card.append(p);
 
