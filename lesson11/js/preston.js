8435768d4c552e788b292465a39b8c6020a40c08
RSwenlin <Robyn796@mac.com>
2021-06-30 20:58:24 -0600

    toggle

--- lesson11/js/preston.js
+++ lesson11/js/preston.js
@@ -1,3 +1,14 @@
 const date = new Date();
 document.querySelector("#theyear").innerHTML = date.getFullYear();
 document.querySelector("#lastmod").innerHTML = document.lastModified;
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
