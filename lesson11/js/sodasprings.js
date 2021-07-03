256860ab9771d40bc3b2aa4dbda87c34cb838f24
RSwenlin <Robyn796@mac.com>
2021-07-02 22:49:36 -0600

    weather summary finalize

--- lesson11/js/sodasprings.js
+++ lesson11/js/sodasprings.js
@@ -12,3 +12,22 @@ hambutton.addEventListener(
   },
   false
 );
+
+const cityid = "5607916";
+const APPID = "36e656b2905c89b5bb9203c9be1b4077";
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const temperature = document.querySelector("#temp");
+    temperature.textContent = jsObject.main.temp.toFixed(0);
+    const currently = document.querySelector("#current");
+    currently.textContent = jsObject.main.feels_like.toFixed(0);
+    const humidity = document.querySelector("#humidity");
+    humidity.textContent = jsObject.main.humidity;
+    const windspeed = document.querySelector("#wind");
+    windspeed.textContent = jsObject.wind.speed.toFixed(0);
+    // const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+  });
