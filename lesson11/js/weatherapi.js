544cd0857dcdf87e7cd366fce9545719c5e6d18b
RSwenlin <Robyn796@mac.com>
2021-07-02 21:58:01 -0600

    weather summary

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -7,12 +7,12 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     const temperature = document.querySelector("#temp");
-    temperature.textContent = jsObject.main.temp;
+    temperature.textContent = jsObject.main.temp.toFixed(0);
     const currently = document.querySelector("#current");
     currently.textContent = jsObject.weather.description;
     const humidity = document.querySelector("#humidity");
     humidity.textContent = jsObject.main.humidity;
     const windspeed = document.querySelector("#wind");
-    windspeed.textContent = jsObject.wind.speed;
+    windspeed.textContent = jsObject.wind.speed.toFixed(0);
     // const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
   });
