cd762388fc5fb1dba83c8c0b6ffa1b533aa90935
RSwenlin <Robyn796@mac.com>
2021-07-05 09:13:53 -0600

    forecast

--- lesson11/js/sodaspringsfiveday.js
+++ lesson11/js/sodaspringsfiveday.js
@@ -6,14 +6,17 @@ const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&AP
 fetch(apiUrl)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    // console.log(jsObject);
 
-    // let day = 0;
-    // const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = 0;
+    const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     // filter the list array for the five day forecasts
     const fiveDayForecast = jsObject.list.filter((forecast) => forecast.dt_txt);
     fiveDayForecast.forEach((x) => {
       let d = new Date(x.dt_txt);
-      document.querySelector(`dayofweek`);
+      document.querySelector(`dayofweek${day + 1}`).textContent =
+        dayofWeek[d.getDay()];
+      document.querySelector(`forecast${day + 1}`).textContent = x.main.temp;
+      day++;
     });
   });
