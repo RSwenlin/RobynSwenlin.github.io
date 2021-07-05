1f812c6367bb4c225da1ad1062c83f7faac9a66d
RSwenlin <Robyn796@mac.com>
2021-07-05 08:20:20 -0600

    lost

--- lesson11/js/sodasprings.js
+++ lesson11/js/sodasprings.js
@@ -30,25 +30,7 @@ fetch(apiURL)
     const windspeed = document.querySelector("#wind");
     windspeed.textContent = jsObject.wind.speed.toFixed(0);
   });
-// const cityid = "5607916";
-// const APPID = "36e656b2905c89b5bb9203c9be1b4077";
 
-// const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&APPID=${APPID}&units=imperial`;
-
-// fetch(apiUrl)
-//   .then((response) => response.json())
-//   .then((jsObject) => {
-
-// // let day = 0;
-// // const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-// // filter the list array for the five day forecasts
-// const fiveDayForecast = jsObject.list.filter( forecast => forecast.dt_txt);
-// fiveDayForecast.forEach(x => {
-// let d =new Date(x.dt_txt);
-// document.querySelector(`dayofweek`)
-// });
-
-// });
 const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
