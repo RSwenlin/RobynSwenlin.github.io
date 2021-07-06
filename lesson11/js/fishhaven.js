6620b0fca796dc98bee6e866c2b0cabfaf700cc0
RSwenlin <Robyn796@mac.com>
2021-07-06 09:09:13 -0600

    trying to fix fiveday

--- lesson11/js/fishhaven.js
+++ lesson11/js/fishhaven.js
@@ -54,3 +54,20 @@ fetch(requestURL)
       document.querySelector("div.eventcards").append(card);
     });
   });
+// fetch(apiUrl)
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+
+//     let day = 0;
+//     const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//     // filter the list array for the five day forecasts
+//     const fiveDayForecast = jsObject.list.filter((forecast) => forecast.dt_txt);
+//     fiveDayForecast.forEach((x) => {
+//       let d = new Date(x.dt_txt);
+//       document.querySelector(`dayofweek${day + 1}`).textContent =
+//         dayofWeek[d.getDay()];
+//       document.querySelector(`forecast${day + 1}`).textContent = x.main.temp;
+//       day++;
+//     });
+//   });
