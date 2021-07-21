b571fbd795a907070ba6643bb3536554b96c7207
Robyn <Robyn796@mac.com>
2021-07-21 11:09:12 -0600

    json

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -9,3 +9,36 @@ hambutton.addEventListener(
   },
   false
 );
+
+const directory = "json/final.json";
+
+fetch(directory)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const business = jsonObject["business"];
+    for (let i = 0; i < business.length; i++) {
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let h3 = document.createElement("h3");
+
+      let p = document.createElement("p");
+      let img = document.createElement("img");
+
+      h2.textContent = `${business[i].name}`;
+
+      h3.textContent = `${business[i].owner}`;
+
+      p.textContent = `${business[i].address}`;
+
+      img.setAttribute("src", business[i].imageurl);
+
+      card.append(h2);
+      card.append(h3);
+      card.append(p);
+      card.append(img);
+
+      document.querySelector("div.cards").append(card);
+    }
+  });
