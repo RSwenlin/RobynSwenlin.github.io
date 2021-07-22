534e781caa0d869776b12478267c168d84e8307a
Robyn <Robyn796@mac.com>
2021-07-21 22:41:47 -0600

    just more

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -2,13 +2,13 @@ const date = new Date();
 document.querySelector("#theyear").innerHTML = date.getFullYear();
 document.querySelector("#lastmod").innerHTML = document.lastModified;
 
-hambutton.addEventListener(
-  "click",
-  () => {
-    mainnav.classList.toggle("responsive");
-  },
-  false
-);
+// hambutton.addEventListener(
+//   "click",
+//   () => {
+//     mainnav.classList.toggle("responsive");
+//   },
+//   false
+// );
 
 const directory = "json/final.json";
 
@@ -17,12 +17,13 @@ fetch(directory)
     return response.json();
   })
   .then(function (jsonObject) {
+    // console.log(jsonObject);
     const business = jsonObject["business"];
     for (let i = 0; i < business.length; i++) {
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let h3 = document.createElement("h3");
-
+      let h4 = document.createElement("h4");
       let p = document.createElement("p");
       let img = document.createElement("img");
 
@@ -30,12 +31,15 @@ fetch(directory)
 
       h3.textContent = `${business[i].owner}`;
 
+      h4.textContent = `${business[i].phone}`;
+
       p.textContent = `${business[i].address}`;
 
       img.setAttribute("src", business[i].imageurl);
 
       card.append(h2);
       card.append(h3);
+      card.append(h4);
       card.append(p);
       card.append(img);
 
